a951213a351bd685b696be985d8fdee3d24743c8
sweatone <chas0302@naver.com>
2023-08-11 09:57:25 +0900

    Initial commit

---
+++ src/main/java/dev/cuffshop/CuffshopApplication.java
@@ -0,0 +1,13 @@
+package dev.cuffshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuffshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuffshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cuffshop/CuffshopApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cuffshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuffshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
