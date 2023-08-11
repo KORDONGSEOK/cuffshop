c1e34a99b8a67f8c7fdb56f501ed846e0d47f585
sweatone <chas0302@naver.com>
2023-08-11 14:09:39 +0900

    상품도메인 구현

--- src/main/java/dev/cuffshop/domain/item/Item.java
+++ src/main/java/dev/cuffshop/domain/item/Item.java
@@ -1,9 +1,21 @@
 package dev.cuffshop.domain.item;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class Item {
     private Long id;
-    private String itemName; //상품명
-    //가격
-    //재고수량
-    //할인율
+    private String itemName;    //상품명
+    private Integer price;      // 가격
+    private Integer quantity;    //재고수량
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
--- src/main/java/dev/cuffshop/repository/ItemRepository.java
+++ src/main/java/dev/cuffshop/repository/ItemRepository.java
@@ -1,4 +1,17 @@
 package dev.cuffshop.repository;
 
-public class ItemRepository {
+import dev.cuffshop.domain.item.Item;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemRepository {
+    Item save(Item item);
+
+    Item findById(Long id);
+
+    List<Item> findAll();
+
+    void update(Long itemId, Item updateParam);
 }
